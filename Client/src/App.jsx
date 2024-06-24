a82fa5398e8a1dbc26c88499addeebf7a43c10ac
Clement Rubiato <91044246+menxio@users.noreply.github.com>
2024-06-24 23:05:44 +0800

    Revert "Transaction feature "

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import 'primereact/resources/themes/saga-blue/theme.css';  //theme
 import 'primereact/resources/primereact.min.css';          //core css
 import 'primeicons/primeicons.css';                        //icons
 import Dashboard from './Components/Dashboard';
-import Transactions from './Components/Transactions';
 import AdminRegister from './Components/AdminRegister';
 import AdminLogin from './Components/AdminLogin';
 import PrivateRoute from './Components/PrivateRoute';
@@ -15,14 +14,26 @@ function App() {
   return (
     <Router>
       <div className="App">
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/admin/register">Admin Register</Link>
+            </li>
+            <li>
+              <Link to="/admin/login">Admin Login</Link>
+            </li>
+          </ul>
+        </nav> */}
         <Routes>
           <Route path="/register" element={<AdminRegister />} />
           <Route path="/login" element={<AdminLogin />} />
           <Route element={<PrivateRoute />}>
-              <Route path="/" element={<Dashboard />}>
-              <Route path="/transactions" element={<Transactions />} />
-              <Route path="/admins" element={<Admins />} />
-           </Route>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/admins" element={<Admins />} />
+          </Route>
         </Routes>
       </div>
     </Router>
--- Client/src/Components/Transactions.jsx
+++ Client/src/Components/Transactions.jsx
@@ -1,17 +1,12 @@
+// Dashboard.jsx
 import React from 'react';
 import TransactionsTable from './TransactionTable';
-import './css/transactions.css';
 
 const Transactions = () => {
-  const sampleTransactions = [
-    { _id: 1, userID: '1234', cardID: '5678', balance: 50, amount: 100, type: 'Credit', status: 'Completed', description: 'NFC Card Top-up', createdAt: '2024-06-20T20:34:56' },
-    { _id: 2, userID: '5678', cardID: '1234', balance: 20, amount: 50, type: 'Debit', status: 'Completed', description: 'Purchase', createdAt: '2024-06-21T15:24:56' },
-  ];
-
   return (
-    <div className="main-content">
+    <div>
       <h1>Transactions</h1>
-      <TransactionsTable transactions={sampleTransactions} />
+      <TransactionsTable />
     </div>
   );
 };
