02bf4f6142c3e135f6f00ee2e4524f108fe944e4
GitShrank <146685295+GitShrank@users.noreply.github.com>
2024-06-24 17:41:38 +0800

    Main content
    width

--- Client/src/Components/TransactionTable.jsx
+++ Client/src/Components/TransactionTable.jsx
@@ -8,11 +8,17 @@ const TransactionsTable = ({ transactions }) => {
         <thead>
           <tr>
             <th>ID</th>
+            <th className='thspaces'></th>
             <th>Date</th>
+            <th className='thspaces'></th>
             <th>User</th>
+            <th className='thspaces'></th>
             <th>Amount</th>
+            <th className='thspaces'></th>
             <th>Type</th>
+            <th className='thspaces'></th>
             <th>Status</th>
+            <th className='thspaces'></th>
             <th>Actions</th>
           </tr>
         </thead>
@@ -20,11 +26,17 @@ const TransactionsTable = ({ transactions }) => {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.id}</td>
+              <td className='spaces'></td>
               <td>{transaction.date}</td>
+              <td className='spaces'></td>
               <td>{transaction.user}</td>
+              <td className='spaces'></td>
               <td>{transaction.amount}</td>
+              <td className='spaces'></td>
               <td>{transaction.type}</td>
+              <td className='spaces'></td>
               <td>{transaction.status}</td>
+              <td className='spaces' ></td>
               <td>
                 <button className="details-button">Details</button>
               </td>
