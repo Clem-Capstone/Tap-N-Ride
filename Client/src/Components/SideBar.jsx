dfcf44a91c89151b83b02901357dd12fc1a86a04
GitShrank <146685295+GitShrank@users.noreply.github.com>
2024-06-25 01:54:55 +0800

    Update SideBar.jsx

--- Client/src/Components/SideBar.jsx
+++ Client/src/Components/SideBar.jsx
@@ -2,33 +2,33 @@
 import React from 'react';
 import './css/sideBar.css';
 
-function SideBar() {   
-  return (
-    <aside id='sidebar' className='sidebar'>
-      <ul className='sidebar-nav' id='sidebar-nav'>
-        <li className="nav-item">
-          <a className="nav-link" href="/dashboard">
-            <i className="bi bi-grid"></i>
-            <span>Dashboard</span>
-          </a>
-        </li>
+function SideBar() {
+    return <aside id='sidebar' className='sidebar'>
+        <ul className='sidebar-nav' id='sidebar-nav'>
+            <li className="nav-item">
+                <a className="nav-link" href="/">
+                    <i className="bi bi-grid"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
 
-        <li className="nav-item">
-          <a className="nav-link" href="/transactions">
-            <i className="bi bi-cash-coin"></i>
-            <span>Transactions</span>
-          </a>
-        </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/transactions">
+                    <i className="bi bi-cash-coin"></i>
+                    <span>Transactions</span>
+                </a>
+            </li>
 
-        <li className="nav-item">
-          <a className="nav-link" href="/admins">
-            <i className="bi bi-person-lock"></i>
-            <span>Admins</span>
-          </a>
-        </li>
-      </ul>
-    </aside>
-  );
+            <li className="nav-item">
+                <a className="nav-link" href="/admins">
+                    <i className="bi bi-person-lock"></i>
+                    <span>Admins</span>
+                </a>
+            </li>
+
+        </ul>
+    </aside>;
+  
 }
 
 export default SideBar;
