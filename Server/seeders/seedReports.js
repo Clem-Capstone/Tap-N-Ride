78adfc58b61bb9ef7e5e18db2b0e550f35071639
GitShrank <146685295+GitShrank@users.noreply.github.com>
2024-07-03 03:57:54 +0800

    seeders from to destination

--- Server/seeders/seedReports.js
+++ Server/seeders/seedReports.js
@@ -1,34 +1,105 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import createReport from "../factories/reportFactory.js";
 import Report from "../models/Report.js";
 
 dotenv.config();
 
-const mongoURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongoURI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+const locations = [
+  "CAGAYAN",
+  "PUERTO",
+  "MAMBATANGAN",
+  "HAITUAN",
+  "LANAO",
+  "ALA-E",
+  "LONOCA",
+  "SAN MIGUEL",
+  "DALIRIG",
+  "CR. DAMAY",
+  "SAN VICENTE",
+  "KISOL-ON",
+  "CR. IMPASUG-ONG",
+  "LA FORTUNA",
+  "PANTULANAN",
+  "IMPALUTAO",
+  "ST. CALIX",
+  "DAWINGAN",
+  "PATPAT",
+  "KALASUNGAY",
+  "SUMIPONG",
+  "MALAYBALAY",
+  "CASISANG",
+  "SAN JOSE",
+  "LAGUITAS",
+  "KIOCAB",
+  "CR. AGLAYAN",
+  "CABANGAHAN",
+  "BANGCUD/NASULI",
+  "DABONGDABONG",
+  "BAGONTAAS",
+  "VALENCIA",
+  "LUMBO",
+  "MUSUAN",
+  "DOLOGON",
+  "TUBIGON",
+  "BAYABASON",
+  "PANADTALAN",
+  "MARAMAG",
+  "CAMP 1",
+  "SINANGUYAN",
+  "DON CARLOS",
+  "KITAOTAO",
+  "LOURDES",
+  "DANGCAGAN",
+  "BARONGCOT",
+  "KIBAWEE",
+  "LABUAGON",
+  "PARAGOPAK",
+  "SPRING",
+  "SAMPAGAR",
+  "OLD DAMULOG",
+  "DAMILOG",
+  "BANBAN",
+  "CUJUANGCO",
+  "MOLETA",
+  "CONCEPTION",
+  "KALAW-KALAW",
+  "MASIMAG",
+  "KALUBIHON",
+  "BOKBOK",
+  "KIARA",
+  "KADINGILAN",
+];
+
+const getRandomLocation = () =>
+  locations[Math.floor(Math.random() * locations.length)];
+
 const seedReports = async () => {
   try {
-    await Report.deleteMany(); // Clear existing data
+    await mongoose.connection.dropCollection("reports");
+  } catch (error) {
+    console.error("No existing reports collection to drop.");
+  }
 
-    const reports = [];
+  const reports = Array.from({ length: 50 }, () => ({
+    busNumber: Math.floor(Math.random() * 10000).toString(),
+    driver: `Driver ${Math.floor(Math.random() * 100)}`,
+    conductor: `Conductor ${Math.floor(Math.random() * 100)}`,
+    passengerFrom: getRandomLocation(),
+    from: getRandomLocation(),
+    to: getRandomLocation(),
+  }));
 
-    for (let i = 0; i < 10; i++) {
-      const report = createReport();
-      reports.push(report);
-    }
-
+  try {
     await Report.insertMany(reports);
     console.log("Reports seeded successfully!");
     mongoose.connection.close();
   } catch (error) {
     console.error("Seeding reports failed:", error);
-    mongoose.connection.close();
   }
 };
 
